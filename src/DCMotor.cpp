6839bbc5e5ecdc1d2f45fb105f0e6edf9abe4f58
Fabien-B <fabien.bonneval@gmail.com>
2019-02-13 21:03:41 +0100

    Adds speedControl mode.

--- src/DCMotor.cpp
+++ src/DCMotor.cpp
@@ -15,36 +15,32 @@ constexpr float DCMotor::INC_PER_MM;
 constexpr float DCMotor::KP;
 constexpr float DCMotor::KI;
 constexpr float DCMotor::KD;
-constexpr int DCMotor::MIN_INTEGRAL;
-constexpr int DCMotor::MAX_INTEGRAL;
+constexpr float DCMotor::MIN_INTEGRAL;
+constexpr float DCMotor::MAX_INTEGRAL;
 constexpr float DCMotor::Z_MAX;
+constexpr float DCMotor::KP_SPEED;
+constexpr float DCMotor::KI_SPEED;
 
 DCMotor * dcmotor = nullptr;
 
-DCMotor::DCMotor() : _isHoming(false), _inc(0), _goal_inc(0), _prevError(0), _integralError(0){
-}
-
-void DCMotor::home(){
-	if (digitalRead(VERTICAL_LIMIT_SWITCH)){
-		// Motor already in limit. Just go down until switch deactivates, then go up
-		_inc = 0;
-		_isHoming = true;
-		_goal_inc = 500 * INC_PER_MM;
-	}else{
-		// Motor not in limit, go up until switch is activated
-		_goal_inc = -500 * INC_PER_MM;
-		_isHoming = false;
-	}
+DCMotor::DCMotor() : _inc(0), _prev_inc(0),
+		_goal(0), _speed(0), _prevError(0), _integralError(0),
+		_speedSetPoint(0), _speedControlActivated(true), _limitReached(false), _intSpeedError(0){
 }
 
 void DCMotor::stop(){
-	_goal_inc = _inc;
+	_speedSetPoint = 0;
+	_goal = _inc / INC_PER_MM;
 	_prevError = 0;
 	_integralError = 0;
+	_intSpeedError = 0;
+	digitalWrite(VERTICAL_MOTOR_DIR_A, 0);
+	digitalWrite(VERTICAL_MOTOR_DIR_B, 0);
+	analogWrite(VERTICAL_MOTOR_PWM, 0);
 }
 
 void DCMotor::go_to(float position){
-	_goal_inc = clamp((float)0, position * INC_PER_MM, Z_MAX * INC_PER_MM);
+	_goal = clamp((float)0, position, Z_MAX);
 }
 
 float DCMotor::get_position(){
@@ -52,7 +48,12 @@ float DCMotor::get_position(){
 }
 
 bool DCMotor::isMoving(){
-	return abs(_inc - _goal_inc) >= 10;
+	if(_speedControlActivated) {
+		return abs(_speedSetPoint) >= 1.0;
+	} else {
+		return abs(_goal - _inc/INC_PER_MM) > 1.0;
+	}
+			//TODO 0.2
 }
 
 void DCMotor::setup(){
@@ -66,31 +67,60 @@ void DCMotor::setup(){
 	analogWriteFrequency(VERTICAL_MOTOR_PWM, 549.3164);
 	attachInterrupt(VERTICAL_ENCODER_A, ISR_INC1, RISING);
 	attachInterrupt(VERTICAL_ENCODER_B, ISR_INC2, RISING);
-	attachInterrupt(VERTICAL_LIMIT_SWITCH, ISR_STOP, RISING);
-	home();
+	attachInterrupt(VERTICAL_LIMIT_SWITCH, ISR_LIMIT_SWITCH, CHANGE);
+	_limitReached = digitalRead(VERTICAL_LIMIT_SWITCH) ? true : false;
 }
 
 void DCMotor::loop(){
+	if(_speedControlActivated) {
+		speedControl();
+	} else {
+		positionControl();
+	}
+}
+
+void DCMotor::speedControl() {
 	cli();
 	int inc = _inc;
 	sei();
-	int error = _goal_inc - inc;
-	int derivError = error - _prevError;
+	_speed = (inc - _prev_inc) / (DC_MOTOR_PERIOD * INC_PER_MM);
+	float speedError = _speed - _speedSetPoint;
+	_intSpeedError = clamp((float)-255, _intSpeedError + speedError, (float)255);
+	float command = speedError * KP_SPEED + _intSpeedError * KI_SPEED;
+	int boundedCommand = clamp(-255, (int)command, 255);
+
+	if(_speedSetPoint == 0) {
+		boundedCommand = 0;
+		_intSpeedError = 0;
+	}
+	digitalWrite(VERTICAL_MOTOR_DIR_A, !sig(boundedCommand));
+	digitalWrite(VERTICAL_MOTOR_DIR_B, sig(boundedCommand));
+	analogWrite(VERTICAL_MOTOR_PWM, abs(boundedCommand));
+
+	_prev_inc = inc;
+}
+
+void DCMotor::positionControl() {
+	cli();
+	int inc = _inc;
+	sei();
+	float error = _goal - inc/INC_PER_MM;
+	float derivError = error - _prevError;
 	_integralError = clamp(MIN_INTEGRAL, _integralError + error, MAX_INTEGRAL);
 	_prevError = error;
 
 	float command = KP * error + KI * _integralError + KP * derivError;
 	int boundedCommand = clamp(-255, (int)command, 255);
 
+	Serial.print(error);
+	Serial.print("\t");
+	Serial.print(command);
+	Serial.print("\t");
+	Serial.println(inc);
+
 	digitalWrite(VERTICAL_MOTOR_DIR_A, sig(boundedCommand));
 	digitalWrite(VERTICAL_MOTOR_DIR_B, !sig(boundedCommand));
 	analogWrite(VERTICAL_MOTOR_PWM, abs(boundedCommand));
-
-	if (_isHoming && !digitalRead(VERTICAL_LIMIT_SWITCH)){
-		// Was homing and now the limit switch is off, go up until it turns on again.
-		_isHoming = false;
-		home();
-	}
 }
 
 void DCMotor::isr_inc1(){
@@ -109,13 +139,6 @@ void DCMotor::isr_inc2(){
 	}
 }
 
-void DCMotor::isr_limit_switch(){
-	_inc = 0;
-	_goal_inc = 0;
-	_prevError = 0;
-	_integralError = 0;
-}
-
 void ISR_INC1(){
 	dcmotor->isr_inc1();
 }
@@ -124,6 +147,12 @@ void ISR_INC2(){
 	dcmotor->isr_inc2();
 }
 
-void ISR_STOP(){
-	dcmotor->isr_limit_switch();
+void ISR_LIMIT_SWITCH(){
+	if(digitalRead(VERTICAL_LIMIT_SWITCH)) {
+		dcmotor->setLimitReached(true);
+	} else {
+		dcmotor->setLimitReached(false);
+	}
+
 }
+
--- src/DCMotor.h
+++ src/DCMotor.h
@@ -15,11 +15,9 @@ public:
 
 	void isr_inc1();
 	void isr_inc2();
-	void isr_limit_switch();
 
 	float get_position();
 	void go_to(float position);
-	void home();
 
 	void setup();
 	void loop();
@@ -28,24 +26,62 @@ public:
 
 	bool isMoving();
 
-private:
-	static constexpr float INC_PER_MM = 50;
-	static constexpr float KP = 5;
-	static constexpr float KI = 0;
-	static constexpr float KD = 0;
-	static constexpr int MIN_INTEGRAL = -255;
-	static constexpr int MAX_INTEGRAL = 255;
-	static constexpr float Z_MAX = 155;
-	bool _isHoming;
-	volatile long _inc;
-	int _goal_inc;
+	bool isSpeedControlActivated() const {
+		return _speedControlActivated;
+	}
 
-	int _prevError;
-	int _integralError;
+	void setSpeedControlActivated(bool positionControlActivated) {
+		_speedControlActivated = positionControlActivated;
+	}
+
+	void setSpeedSetPoint(float speedSetPoint) {
+		_speedSetPoint = speedSetPoint;
+	}
+
+	bool isLimitReached() const {
+		return _limitReached;
+	}
+
+	void setLimitReached(bool limitReached) {
+		_limitReached = limitReached;
+	}
+
+	void setHome() {
+		_inc = 0;
+	}
+
+private:
+	static constexpr float INC_PER_MM = 55.5;
+	static constexpr float KP = 60;
+	static constexpr float KI = 1;
+	static constexpr float KD = 0;
+	static constexpr float KP_SPEED = 10;
+	static constexpr float KI_SPEED = 3;
+	static constexpr float MIN_INTEGRAL = -255;
+	static constexpr float MAX_INTEGRAL = 255;
+	static constexpr float Z_MAX = 155;
+
+
+	void speedControl();
+
+	void positionControl();
+
+	volatile long _inc;
+	long _prev_inc;
+	float _goal;
+	float _speed;
+
+	float _prevError;
+	float _integralError;
+
+	float _speedSetPoint;
+	bool _speedControlActivated;
+	bool _limitReached;
+	float _intSpeedError;
 };
 
 void ISR_INC1();
 void ISR_INC2();
-void ISR_STOP();
+void ISR_LIMIT_SWITCH();
 
 #endif /* DCMOTOR_H_ */
